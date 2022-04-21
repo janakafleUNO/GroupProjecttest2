0924598e13020ae0130161e2471ef1fdc9169adc
janakafleUNO <jkafle@unomaha.edu>
2022-04-20 21:24:10 -0500

    Aded the code

--- GroupProject/settings.py
+++ GroupProject/settings.py
@@ -119,8 +119,7 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+
 
 LOGIN_URL = 'login'
 # Default primary key field type
